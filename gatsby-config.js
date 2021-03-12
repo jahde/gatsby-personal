4da6fcd5fb108b66c4b1e2141e9b08fb5e95cbcf
jahde <jahde441@gmail.com>
2021-03-12 17:30:41 -0400

    update favicons

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,9 +50,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
-        short_name: `minimal-blog`,
-        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
+        name: `jahde-blog - @jahde/gatsby-personal`,
+        short_name: `jahde-blog`,
+        description: `Engineer, Entrepreneur and Explorer from the island of Bermuda.`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#6B46C1`,
